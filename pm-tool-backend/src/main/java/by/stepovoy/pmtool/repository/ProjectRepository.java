a41c5267fd5bc6021dc89a7894758d9397852764
Vladislav <vladstepovoyvios@gmail.com>
2020-06-25 19:28:15 +0300

    added Project repository

---
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package by.stepovoy.pmtool.repository;
+
+import by.stepovoy.pmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
