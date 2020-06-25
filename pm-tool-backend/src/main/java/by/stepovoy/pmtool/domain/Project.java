8e2c9ea50c590b6004660b11277913509a23929a
Vladislav <vladstepovoyvios@gmail.com>
2020-06-25 19:21:08 +0300

    updated Project entity

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/domain/Project.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/domain/Project.java
@@ -1,4 +1,35 @@
 package by.stepovoy.pmtool.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
 }
