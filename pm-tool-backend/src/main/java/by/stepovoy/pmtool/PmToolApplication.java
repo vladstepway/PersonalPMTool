ae55f03ecbd2195c3a420559d6611d6400b4557c
Vladislav <vladstepovoyvios@gmail.com>
2020-06-25 17:57:08 +0300

    Added dependencies

---
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/PmToolApplication.java
@@ -0,0 +1,13 @@
+package by.stepovoy.pmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmToolApplication.class, args);
+    }
+
+}
