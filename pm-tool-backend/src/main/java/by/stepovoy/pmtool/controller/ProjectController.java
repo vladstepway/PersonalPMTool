f60d5b4fb6b12c198d559904b8944410f54d71ce
Vladislav <vladstepovoyvios@gmail.com>
2020-06-27 00:15:37 +0300

    added validation service

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/controller/ProjectController.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/controller/ProjectController.java
@@ -2,38 +2,35 @@ package by.stepovoy.pmtool.controller;
 
 import by.stepovoy.pmtool.domain.Project;
 import by.stepovoy.pmtool.service.ProjectService;
+import by.stepovoy.pmtool.service.ValidationErrorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final ValidationErrorService validationErrorService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, ValidationErrorService validationErrorService) {
         this.projectService = projectService;
+        this.validationErrorService = validationErrorService;
     }
 
     @PostMapping("")
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-
-            Map<String,String> errorMap = new HashMap<>();
-            result.getFieldErrors().forEach(e->errorMap.put(e.getField(),e.getDefaultMessage()));
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = validationErrorService.validate(result);
+        if (errorMap != null) {
+            return errorMap;
         }
 
         Project project1 = projectService.saveOrUpdate(project);
