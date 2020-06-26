51cc72982a6269857cb652750b0b21c5f0b62a35
Vladislav <vladstepovoyvios@gmail.com>
2020-06-27 00:07:18 +0300

    updated createProject method in controller

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/controller/ProjectController.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/controller/ProjectController.java
@@ -5,12 +5,16 @@ import by.stepovoy.pmtool.service.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -26,7 +30,10 @@ public class ProjectController {
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid object", HttpStatus.BAD_REQUEST);
+
+            Map<String,String> errorMap = new HashMap<>();
+            result.getFieldErrors().forEach(e->errorMap.put(e.getField(),e.getDefaultMessage()));
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project project1 = projectService.saveOrUpdate(project);
