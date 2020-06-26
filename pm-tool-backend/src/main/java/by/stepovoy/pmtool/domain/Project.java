5764dce43286bdcb6e23ac899236965ddffb79da
Vladislav <vladstepovoyvios@gmail.com>
2020-06-26 22:23:23 +0300

    added validation to Project object

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/domain/Project.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/domain/Project.java
@@ -1,8 +1,17 @@
 package by.stepovoy.pmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -13,13 +22,21 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4,max = 5, message = "Please, use 4 to 5 characters")
+    @Column(updatable = false,unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
