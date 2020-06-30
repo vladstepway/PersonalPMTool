3cdd83b308e5cfcce92c8359afdd5208e9c2d3ca
Vladislav <vladstepovoyvios@gmail.com>
2020-07-01 00:08:18 +0300

    added getting all projects from the server

---
+++ pm-tool-frontend-react/src/reducers/projectReducer.js
@@ -0,0 +1,16 @@
+import { GET_PROJECTS } from "../actions/types";
+
+const initialState = {
+  projects: [],
+  project: {},
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PROJECTS:
+      return { ...state, projects: action.payload };
+
+    default:
+      return state;
+  }
+};
