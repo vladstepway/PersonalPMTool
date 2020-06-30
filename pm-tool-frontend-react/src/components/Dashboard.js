dbd852b6d19ebdf941d8cc37880d7e2ed580a1a4
Vladislav <vladstepovoyvios@gmail.com>
2020-06-30 11:57:03 +0300

    added dashboard component

--- pm-tool-frontend-react/src/components/Dashboard.js
+++ pm-tool-frontend-react/src/components/Dashboard.js
@@ -4,11 +4,62 @@ import ProjectItem from "./Project/ProjectItem";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+      <div class="projects">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h1 class="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" class="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+
+              {/* {<!-- Project Item Component -->} */}
+              <div class="container">
+                <div class="card card-body bg-light mb-3">
+                  <div class="row">
+                    <div class="col-2">
+                      <span class="mx-auto">REACT</span>
+                    </div>
+                    <div class="col-lg-6 col-md-4 col-8">
+                      <h3>Spring / React Project</h3>
+                      <p>
+                        Project to create a Kanban Board with Spring Boot and
+                        React
+                      </p>
+                    </div>
+                    <div class="col-md-4 d-none d-lg-block">
+                      <ul class="list-group">
+                        <a href="#">
+                          <li class="list-group-item board">
+                            <i class="fa fa-flag-checkered pr-1">
+                              Project Board{" "}
+                            </i>
+                          </li>
+                        </a>
+                        <a href="#">
+                          <li class="list-group-item update">
+                            <i class="fa fa-edit pr-1">Update Project Info</i>
+                          </li>
+                        </a>
+                        <a href="">
+                          <li class="list-group-item delete">
+                            <i class="fa fa-minus-circle pr-1">
+                              Delete Project
+                            </i>
+                          </li>
+                        </a>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* {<!-- End of Project Item Component -->} */}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
