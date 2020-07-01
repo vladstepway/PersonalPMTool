964394c141a8350863b1411c5fa7d470be95f428
Vladislav <vladstepovoyvios@gmail.com>
2020-07-01 17:35:57 +0300

    added backlog entity and repository

---
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/domain/Backlog.java
@@ -0,0 +1,26 @@
+package by.stepovoy.pmtool.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Backlog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence=0;
+    private String projectIdentifier;
+
+    //OneToOne with project
+
+    //OneToMany with project tasks
+
+}
