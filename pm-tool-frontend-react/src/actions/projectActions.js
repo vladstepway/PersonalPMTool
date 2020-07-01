682b4da9fb4dc4aaa994f074c0381150379c6b4f
Vladislav <vladstepovoyvios@gmail.com>
2020-07-01 16:57:42 +0300

    added all CRUD operations with proxying api

--- pm-tool-frontend-react/src/actions/projectActions.js
+++ pm-tool-frontend-react/src/actions/projectActions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
-    const res = await axios.post("http://localhost:8080/api/projects", project);
+    const res = await axios.post("/api/projects", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
@@ -18,15 +18,26 @@ export const createProject = (project, history) => async (dispatch) => {
 };
 
 export const getProjects = () => async (dispatch) => {
-  const res = await axios.get("http://localhost:8080/api/projects");
+  const res = await axios.get("/api/projects");
   dispatch({ type: GET_PROJECTS, payload: res.data });
 };
 
 export const getProject = (id, history) => async (dispatch) => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/projects/${id}`);
+    const res = await axios.get(`/api/projects/${id}`);
     dispatch({ type: GET_PROJECT, payload: res.data });
   } catch (err) {
     history.push("/dashboard");
+  }
+};
+
+export const deleteProject = (id) => async (dispatch) => {
+  if (
+    window.confirm(
+      "Are you sure? This will delete the whole project and all data related to it"
+    )
+  ) {
+    await axios.delete(`/api/projects/${id}`);
+    dispatch({ type: DELETE_PROJECT, payload: id });
   }
 };
--- pm-tool-frontend-react/src/reducers/projectReducer.js
+++ pm-tool-frontend-react/src/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS, GET_PROJECT } from "../actions/types";
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
 
 const initialState = {
   projects: [],
@@ -11,6 +11,13 @@ export default (state = initialState, action) => {
       return { ...state, projects: action.payload };
     case GET_PROJECT:
       return { ...state, project: action.payload };
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(
+          (p) => p.projectIdentifier !== action.payload
+        ),
+      };
     default:
       return state;
   }
