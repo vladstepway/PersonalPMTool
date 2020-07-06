48b2e2c8ac1926b0b2ee482b695ce803527becb6
ustsepavy <vladstepovoyvios@gmail.com>
2020-07-06 18:31:01 +0300

    routes changed and added project task creation

--- pm-tool-frontend-react/src/App.js
+++ pm-tool-frontend-react/src/App.js
@@ -1,29 +1,33 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Header />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/add" component={AddProject} />
-            <Route exact path="/update/:id" component={UpdateProject} />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Header/>
+                        <Route exact path="/projects" component={Dashboard}/>
+                        <Route exact path="/projects/add" component={AddProject}/>
+                        <Route exact path="/projects/:id/update" component={UpdateProject}/>
+                        <Route exact path="/projects/:id" component={ProjectBoard}/>
+                        <Route exact path="/projects/:id/tasks/add" component={AddProjectTask}/>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
