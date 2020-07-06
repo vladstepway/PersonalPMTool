fd1a756761aaf3fc243914250f4beb1754944796
ustsepavy <vladstepovoyvios@gmail.com>
2020-07-07 00:45:26 +0300

    refactoring

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/service/ProjectTaskService.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/service/ProjectTaskService.java
@@ -39,7 +39,7 @@ public class ProjectTaskService {
             projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
             projectTask.setProjectIdentifier(projectIdentifier);
             //initial priority when priority null
-            if (projectTask.getPriority() == null) {
+            if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
                 projectTask.setPriority(3);
             }
             //initial status when status null
