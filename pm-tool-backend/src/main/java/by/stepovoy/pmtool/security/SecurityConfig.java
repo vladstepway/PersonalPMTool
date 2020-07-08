977e14ee3e9830cbdf8ee0cdfe841807195e1c66
ustsepavy <vladstepovoyvios@gmail.com>
2020-07-08 14:57:35 +0300

    added api call route for security

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/security/SecurityConfig.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
+                .antMatchers("/api/users/**").permitAll()
                 .anyRequest()
                 .authenticated();
     }
