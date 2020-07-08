a7daa6eb01f6920c47b1e54b9f4f779a0bb3aa69
ustsepavy <vladstepovoyvios@gmail.com>
2020-07-08 14:56:54 +0300

    added BCryptPasswordEncoder bean

--- pm-tool-backend/src/main/java/by/stepovoy/pmtool/PmToolApplication.java
+++ pm-tool-backend/src/main/java/by/stepovoy/pmtool/PmToolApplication.java
@@ -2,9 +2,17 @@ package by.stepovoy.pmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PmToolApplication {
+
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PmToolApplication.class, args);
